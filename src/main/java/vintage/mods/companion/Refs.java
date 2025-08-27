d5973a0516de89a04007a80da50cd1855a0f722e
CrossVas <cross.wanted.1.4@gmail.com>
2025-08-27 09:50:07 +0300

    maintenance: updated metadata

--- src/main/java/vintage/mods/companion/Refs.java
+++ src/main/java/vintage/mods/companion/Refs.java
@@ -6,7 +6,7 @@ import mods.vintage.core.helpers.StackHelper;
 public class Refs {
 
     public static final String id = "vintagecompanion";
-    public static final String name = "Vintage Companion";
+    public static final String name = "Vintage: Companion";
 
     public static boolean shouldAdd(String oreDictValue) {
         return !StackHelper.getStackFromOre(oreDictValue).isEmpty();
