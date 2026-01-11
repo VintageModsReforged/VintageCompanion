4b421f7587980e23721e7c66fdbbae3db4f1325b
CrossVas <cross.wanted.1.4@gmail.com>
2026-01-11 09:48:56 +0200

    fixed: flame particle spawning being too much

--- src/main/java/vintage/mods/companion/utils/ToolAction.java
+++ src/main/java/vintage/mods/companion/utils/ToolAction.java
@@ -1,8 +1,9 @@
 package vintage.mods.companion.utils;
 
-import mods.vintage.core.helpers.ToolHelper;
+import mods.vintage.core.helpers.StackHelper;
+import mods.vintage.core.utils.IHarvestCallback;
 import net.minecraft.block.Block;
-import net.minecraft.entity.item.EntityItem;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.item.EntityXPOrb;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -11,44 +12,61 @@ import net.minecraft.world.World;
 
 public class ToolAction {
 
-    public static ToolHelper.IDropCallback smelt() {
-        return new ToolHelper.IDropCallback() {
+    public static IHarvestCallback smelt() {
+        return new IHarvestCallback() {
             @Override
-            public void handleServer(World world, Block block, int x, int y, int z, int metadata, EntityPlayer player) {
-                boolean dontBreak = false;
-                for (ItemStack input : block.getBlockDropped(world, x, y, z, metadata, 0)) {
-                    ItemStack result = FurnaceRecipes.smelting().getSmeltingResult(input);
-                    if (result != null) {
-                        if (!world.isRemote) {
-                            float floatXP = FurnaceRecipes.smelting().getExperience(result);
-                            int smeltXP = (int) floatXP;
-                            if (floatXP > (float) smeltXP && world.rand.nextFloat() < floatXP - (float) smeltXP) {
-                                ++smeltXP;
-                            }
-
-                            while (smeltXP > 0) {
-                                int splitXP = EntityXPOrb.getXPSplit(smeltXP);
-                                smeltXP -= splitXP;
-                                world.spawnEntityInWorld(new EntityXPOrb(world, (double) x + (double) 0.5F, (double) y + (double) 0.5F, (double) z + (double) 0.5F, splitXP));
-                            }
-                            world.spawnEntityInWorld(new EntityItem(world, (double) x + (double) 0.5F, (double) y + (double) 0.5F, (double) z + (double) 0.5F, result.copy()));
-                        }
-                    } else {
-                        dontBreak = true;
-                        break;
-                    }
-                }
-                if (dontBreak) block.harvestBlock(world, player, x, y, z, metadata);
+            public HarvestMode getMode(World world, Block block, int x, int y, int z, int meta, EntityPlayer player) {
+                return HarvestMode.SMELT;
             }
 
             @Override
-            public void handleClient(World world, Block block, int x, int y, int z, int metadata, EntityPlayer player) {
-                for (int i = 0; i < 5; ++i) {
-                    double rx = world.rand.nextGaussian() * 0.02;
-                    double ry = world.rand.nextGaussian() * 0.02;
-                    double rz = world.rand.nextGaussian() * 0.02;
-                    double magnitude = 20.0;
-                    world.spawnParticle("flame", (double) x + (double) 0.5F + rx * magnitude, (double) y + (double) 0.5F + ry * magnitude, (double) z + (double) 0.5F + rz * magnitude, -rx, -ry, -rz);
+            public void handleCustom(World world, Block block, int x, int y, int z, int meta, EntityPlayer player) {
+                int fortune = EnchantmentHelper.getFortuneModifier(player);
+                boolean silk = EnchantmentHelper.getSilkTouchModifier(player);
+
+                // Silk Touch fallback
+                if (silk) {
+                    block.harvestBlock(world, player, x, y, z, meta);
+                    return;
+                }
+
+                boolean droppedSomething = false;
+
+                for (ItemStack input : block.getBlockDropped(world, x, y, z, meta, fortune)) {
+                    ItemStack smelted = FurnaceRecipes.smelting().getSmeltingResult(input);
+
+                    if (smelted == null) {
+                        // fallback to vanilla
+                        block.harvestBlock(world, player, x, y, z, meta);
+                        return;
+                    }
+
+                    droppedSomething = true;
+
+                    // spawn the smelted item
+                    StackHelper.dropAsEntity(world, x, y, z, smelted.copy());
+
+                    // spawn furnace XP
+                    float xpFloat = FurnaceRecipes.smelting().getExperience(smelted);
+                    int xpInt = (int) xpFloat;
+                    if (xpFloat > xpInt && world.rand.nextFloat() < xpFloat - xpInt) {
+                        xpInt++;
+                    }
+
+                    while (xpInt > 0) {
+                        int split = EntityXPOrb.getXPSplit(xpInt);
+                        xpInt -= split;
+                        world.spawnEntityInWorld(new EntityXPOrb(world, x + 0.5, y + 0.5, z + 0.5, split));
+                    }
+                }
+
+                // fallback if nothing smelted
+                if (!droppedSomething) {
+                    block.harvestBlock(world, player, x, y, z, meta);
+                } else {
+                    if (world.rand.nextFloat() < 0.3F) {
+                        world.playAuxSFX(2004, x, y, z, 0); // furnace flame
+                    }
                 }
             }
         };
